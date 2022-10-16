d061ddf0000ab238309a8539db9ebe6ea04c38f3
OriginVO <110988862+OriginVO@users.noreply.github.com>
2022-10-15 20:52:45 -0500

    Add files via upload

---
+++ PRO-C22-SA-Boilerplate_code-main/js/Cannon.js
@@ -0,0 +1,24 @@
+class Cannon {
+  constructor(x, y, width, height, angle) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = angle;
+    this.cannonImage=loadImage("./assets/canon.png");
+    this.cannonBase=loadImage("./assets/cannonBase.png");
+  }
+  display(){
+    push();
+    
+    imageMode(CENTER);
+    image(this.cannonImage,this.x,this.y,this.width,this.height);
+    translate(this.x,this.y);
+    rotate(this.angle)
+    pop();
+    image(this.cannonBase,70,20,200,200);
+    noFill();
+
+   }
+}
+ 
